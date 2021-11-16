7e09d97746e7b430bb85036c1b095fdec961140a
Cody Zhang <codyzhang4@hotmail.com>
2021-11-15 22:28:41 -0800

    add back some deletions

---
+++ scripts/firebaseAPI_DTC10.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCiozT5t3hKu2nLWrGA1DDxrCKnStRSEZg",
+  authDomain: "dtc-10-comp1800.firebaseapp.com",
+  projectId: "dtc-10-comp1800",
+  storageBucket: "dtc-10-comp1800.appspot.com",
+  messagingSenderId: "707900311043",
+  appId: "1:707900311043:web:f69d56437bb835d1187036",
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
