b87fd1988b9bec62013c6e7e4443797c1e5a7757
Cody Zhang <codyzhang4@hotmail.com>
2021-11-17 16:34:23 -0800

    copying from github didn't work

--- new_gameplay.js
+++ new_gameplay.js
@@ -1,14 +1,9 @@
 // We're initializing firebase here as a test
-
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-
 // //
 //insert score and quizvalue for user
 var currentUser;
-
-
 function insertScoreVals() {
   //check user authentication with firebase
   firebase.auth().onAuthStateChanged(user => {
@@ -16,7 +11,6 @@ function insertScoreVals() {
     if (user) {
       //access the userid fields
       currentUser = db.collection("users").doc(user.uid)
-
       currentUser.get()
         .then(userDoc => {
           // I believe these are local scope variables
@@ -32,7 +26,6 @@ function insertScoreVals() {
           } else {
             console.log("Score present in database.")
           };
-
           if (totalQuizzes == null) {
             currentUser.update({
               quizTotal: 0,
@@ -40,13 +33,15 @@ function insertScoreVals() {
           } else {
             console.log("Quiz Total present in database.")
           }
-
         })
     } else {
       console.log("No user is signed in")
     }
   });
 }
+
+//calls function to ensure scores present in firebase/firestore
+insertScoreVals();
 
 // define constants from the html - start with score and with one button
 // these are global scope (I think)
@@ -57,14 +52,12 @@ let pausebutt = document.querySelector("#pause");
 let playbutton = document.querySelector("#play");
 // This is to run the while loop
 let pause = false;
-
 //display the scores
 function displayScores() {
   //authenticate user
   firebase.auth().onAuthStateChanged(user => {
     //if logged in/exists
     if (user) {
-
       currentUser = db.collection("users").doc(user.uid)
       //check the current score, quiz totals
       currentUser.get()
@@ -72,8 +65,6 @@ function displayScores() {
           //local scope variables
           var currentScore = userDoc.data().score;
           var currentQuiz = userDoc.data().quizTotal;
-          
-
           //this should display the numbers
           score_tally.innerHTML = currentScore;
           quiz_tally.innerHTML = currentQuiz;
@@ -82,6 +73,7 @@ function displayScores() {
           localnews.onclick = () => addPoints(currentUser);
           // New addition - calls function recursively?
           // Would a conditional work here?
+          displayScores();
          // displayScores();
         })
     }
@@ -94,31 +86,14 @@ insertScoreVals();
 //Calls function to run clicker game - this works, but does not update the printout
 displayScores();
 
-// function incrementer(){
-//   firebase.auth().onAuthStateChanged(user => {
-//     //if logged in/exists
-//     if (user) {
-
-//       var i = db.collection("users").doc("quizTotal").get()
-//       console.log(i);
-//       //check the current score, quiz totals
-//       currentUser.get()
-//         .then(userDoc => {
-//         })
-//     }
-//   }) 
-// };
-// incrementer();
-
-var i = 1;
 //increment our main score
 function addPoints(currentUser) {
   console.log("inside");
   //update the value stored in the score field associated with the user
   currentUser.update({
-    score: firebase.firestore.FieldValue.increment(i)
+    score: firebase.firestore.FieldValue.increment(1)
   })
-};
+}; 
 
 
 //Add a listener to update the score. .onsnapshot checks our database for changes.
