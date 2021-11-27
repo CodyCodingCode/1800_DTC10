16f74604f7fab463de6e295c184426932a8b44bb
Wilson Scott <wscott53@my.bcit.ca>
2021-11-26 16:48:47 -0800

    Revise code and style formatting for leader's board

--- leaderboard.js
+++ leaderboard.js
@@ -4,18 +4,12 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // //
 
 //This is the html list in leaderboard.html
-const table = document.querySelectorAll("#leaders");
 const nameHeading = document.querySelector("#name");
 const scoreHeading = document.querySelector("#score");
 const managersHeading = document.querySelector("#managers");
 
 //data injection function
 function createRanks(doc){
-  //this creates our variables to implement into our rankList
-  //let li = document.createElement('li');
-  // let name = document.createElement('td');
-  // let score = document.createElement('td');
-  // let managers = document.createElement('td');
 
   //Create new cells
   nameCell = nameHeading.insertCell();
@@ -23,18 +17,9 @@ function createRanks(doc){
   managerCell = managersHeading.insertCell();
 
   //We set each variable above with information from firebase
-  //li.setAttribute('rankInfo', doc.id);
   nameCell.innerHTML = doc.data().name;
   scoreCell.innerHTML = doc.data().score;
   managerCell.innerHTML = doc.data().manager;
-
-  //We append the now-populated variables to the main li variable in line 18
-  // li.appendChild(name);
-  // li.appendChild(score);
-  // li.appendChild(managers);
-
-  // //We append the list variable to the current html element
-  // rankList.appendChild(li);
 }
 
 //This queries our user collection for the top three (in descending order from largest to least)
