3b3449011739a6046d094a25ae975937715e0ebb
Wilson Scott <wscott53@my.bcit.ca>
2021-11-26 16:41:13 -0800

    revise formatting in leadership table

--- leaderboard.js
+++ leaderboard.js
@@ -4,29 +4,37 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // //
 
 //This is the html list in leaderboard.html
-const rankList = document.querySelector('#ranks');
+const table = document.querySelectorAll("#leaders");
+const nameHeading = document.querySelector("#name");
+const scoreHeading = document.querySelector("#score");
+const managersHeading = document.querySelector("#managers");
 
 //data injection function
 function createRanks(doc){
   //this creates our variables to implement into our rankList
-  let li = document.createElement('li');
-  let name = document.createElement('span');
-  let score = document.createElement('span');
-  let managers = document.createElement('span');
+  //let li = document.createElement('li');
+  // let name = document.createElement('td');
+  // let score = document.createElement('td');
+  // let managers = document.createElement('td');
+
+  //Create new cells
+  nameCell = nameHeading.insertCell();
+  scoreCell = scoreHeading.insertCell();
+  managerCell = managersHeading.insertCell();
 
   //We set each variable above with information from firebase
-  li.setAttribute('rankInfo', doc.id);
-  name.textContent = doc.data().name;
-  score.textContent = doc.data().score;
-  managers.textContent = doc.data().manager;
+  //li.setAttribute('rankInfo', doc.id);
+  nameCell.innerHTML = doc.data().name;
+  scoreCell.innerHTML = doc.data().score;
+  managerCell.innerHTML = doc.data().manager;
 
   //We append the now-populated variables to the main li variable in line 18
-  li.appendChild(name);
-  li.appendChild(score);
-  li.appendChild(managers);
+  // li.appendChild(name);
+  // li.appendChild(score);
+  // li.appendChild(managers);
 
-  //We append the list variable to the current html element
-  rankList.appendChild(li);
+  // //We append the list variable to the current html element
+  // rankList.appendChild(li);
 }
 
 //This queries our user collection for the top three (in descending order from largest to least)
